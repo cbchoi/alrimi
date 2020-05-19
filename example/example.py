d8ab61bd0b70b4b89665774e624b7f9a82c940c4
Changbeom Choi <cbchoi@handong.edu>
2020-05-19 17:22:07 +0900

    writing

--- example/example.py
+++ example/example.py
@@ -6,7 +6,53 @@ from selenium.webdriver.support.wait import WebDriverWait
 from selenium.webdriver.support.ui import Select
 from selenium.webdriver.common.alert import Alert
 
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.common.exceptions import TimeoutException
+
 from urllib import parse
+
+# 게시판 글 읽기
+#browser.get(f"https://hisnet.handong.edu/cis/list.php?Board=KYOM_EXTRA&CateCode=2959")
+
+def post_thread(browser, subject, content):
+	browser.get(f"https://hisnet.handong.edu/cis/write.php?Board=KYOM_EXTRA&CateCode=2959&CIS_GWAMOK=20200101SIT32004&dflag=")
+	browser.find_element_by_name('subject').send_keys(subject)
+
+	#WebElement formElement = driver.findElement(By.name("form_w"));
+	#List<WebElement> allFormChildElements = formElement.findElements(By.xpath("*"));
+	#browser.find_element_by_class_name('content')
+
+	element = browser.find_element_by_name('content')
+	#WebDriverWait(browser, 5).until(
+	#    EC.presence_of_element_located((By.ID, "content"))
+	#)  # Wait until the `text_to_score` element appear (up to 5 seconds)
+	browser.execute_script(f'oEditors.getById["content"].setIR("{content}")')
+	browser.execute_script('CheckFormW(document.form_w)')
+	#browser.switch_to_frame("MyIFrame")
+	browser.implicitly_wait(5)
+
+	try:
+	    WebDriverWait(browser, 3).until(EC.alert_is_present(),
+	                                   'Timed out waiting for PA creation ' +
+	                                   'confirmation popup to appear.')
+
+	    alert = browser.switch_to.alert
+	    alert.accept()
+	    print("alert accepted")
+	except TimeoutException:
+	    print("no alert")
+
+def handle_alert(browser):
+	try:
+	    WebDriverWait(browser, 3).until(EC.alert_is_present(),
+	                                   'Timed out waiting for PA creation ' +
+	                                   'confirmation popup to appear.')
+
+	    alert = browser.switch_to.alert
+	    alert.accept()
+	    print("alert accepted")
+	except TimeoutException:
+	    print("no alert")
 
 options = Options()
 options.headless = False
@@ -18,26 +64,4 @@ browser.find_element_by_name('password').send_keys('sit32004')
 browser.find_element_by_name('login').submit()
 browser.implicitly_wait(3)
 
-# 게시판 글 읽기
-#browser.get(f"https://hisnet.handong.edu/cis/list.php?Board=KYOM_EXTRA&CateCode=2959")
-
-browser.get(f"https://hisnet.handong.edu/cis/write.php?Board=KYOM_EXTRA&CateCode=2959&dflag=")
-browser.find_element_by_name('subject').send_keys('loveetls')
-
-#WebElement formElement = driver.findElement(By.name("form_w"));
-#List<WebElement> allFormChildElements = formElement.findElements(By.xpath("*"));
-#browser.find_element_by_class_name('content')
-from selenium.webdriver.common.by import By
-from selenium.webdriver.support.ui import WebDriverWait
-from selenium.webdriver.support import expected_conditions as EC
-
-
-element = browser.find_element_by_name('content')
-#WebDriverWait(browser, 5).until(
-#    EC.presence_of_element_located((By.ID, "content"))
-#)  # Wait until the `text_to_score` element appear (up to 5 seconds)
-browser.execute_script('oEditors.getById["content"].setIR("11")')
-browser.find_element_by_name('form_w').submit()
-browser.implicitly_wait(3)
-browser.switch_to_frame('MyIFrame').switch_to_alert().accept();
-
+post_thread(browser, "ICT Application Development", "Hello, World!!")
