e634d4c9a985f3bda8899e3b6330d870a4c43a66
SinYoung Kim <21600824@handong.edu>
2020-05-20 15:41:41 +0900

    example

--- example/example1.py
+++ example/example1.py
@@ -10,71 +10,64 @@ from selenium.webdriver.common.by import By
 from selenium.common.exceptions import NoSuchElementException
 from selenium.webdriver.support.ui import WebDriverWait
 
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.common.exceptions import TimeoutException
+
 from urllib import parse
+from instance.credential import *
 
 options = Options()
 options.headless = False
 browser = webdriver.Chrome(executable_path="./chromedriver.exe", options=options)
 browser.get("https://hisnet.handong.edu/login/login.php")
 
-browser.find_element_by_name('id').send_keys('loveetls')
+browser.find_element_by_name('id').send_keys(LOGIN_ID)
 browser.implicitly_wait(3)
-browser.find_element_by_name('password').send_keys('sit32004')
+time.sleep(2)
+browser.find_element_by_name('password').send_keys(LOGIN_PW)
 browser.implicitly_wait(3)
+time.sleep(2)
 browser.find_element_by_name('login').submit()
 browser.implicitly_wait(3)
 
 # 게시판 글 읽기
-time.sleep(5)
+time.sleep(2)
 browser.find_element_by_xpath('//*[@id="td_box32"]').click()
 notice = browser.find_element_by_xpath('//*[@id="tr_box_32"]/table/tbody')
-time.sleep(5)
+time.sleep(2)
 print(notice.text)
 
-time.sleep(5)
+# 과제정보
+time.sleep(2)
 browser.find_element_by_xpath('//*[@id="td_box34"]').click()
-#//*[@id="tr_box_34"]/table/tbody
-'''
-browser.implicitly_wait(3)
-browser.get("https://hisnet.handong.edu/cis/list.php?Board=KYOM_NOTICE&kang_yy=2020&kang_hakgi=1&CIS_GWAMOK=&AG=1")
-notice = browser.find_element_by_name('login')
-print()
+hw = browser.find_element_by_xpath('//*[@id="tr_box_34"]/table/tbody')
+time.sleep(2)
+print(hw.text)
 
-browser.implicitly_wait(3)
-browser.find_element_by_class_name('cls_AlignLeft listBody')
+# 과제 상세리스트
+browser.find_element_by_xpath('/html/body/table[2]/tbody/tr/td[3]/table/tbody/tr[1]/td[2]').click()
+hw_d = browser.find_element_by_xpath('//*[@id="att_list"]/tbody')
+time.sleep(2)
+print(hw_d.text)
 
-# browser.get(f"read.php?id=113301&Page=1&Board=KYOM_EXTRA&CateCode=2959&dflag=")
-browser.navigate().back();
+# 게시판 상세리스트
+browser.back()
+time.sleep(2)
+browser.find_element_by_xpath('//*[@id="td_box32"]').click()
+time.sleep(2)
+browser.find_element_by_xpath('/html/body/table[2]/tbody/tr/td[3]/table/tbody/tr[1]/td[2]').click()
+time.sleep(2)
+notice_d1 = browser.find_element_by_xpath('/html/body/table[1]/tbody/tr[2]/td/table/tbody/tr/td[3]/table/tbody/tr[4]/td/table/tbody/tr[1]')
+print(notice_d1.text)
 
-#WebElement formElement = driver.findElement(By.name("form_w"));
-#List<WebElement> allFormChildElements = formElement.findElements(By.xpath("*"));
-#browser.find_element_by_class_name('content')
-from selenium.webdriver.common.by import By
-from selenium.webdriver.support.ui import WebDriverWait
-from selenium.webdriver.support import expected_conditions as EC
+time.sleep(2)
+browser.find_element_by_xpath('/html/body/table[1]/tbody/tr[2]/td/table/tbody/tr/td[3]/table/tbody/tr[4]/td/table/tbody/tr[4]/td/table/tbody/tr/td/div/a[1]').click()
+time.sleep(2)
+notice_d1 = browser.find_element_by_xpath('/html/body/table[1]/tbody/tr[2]/td/table/tbody/tr/td[3]/table/tbody/tr[4]/td/table/tbody/tr[1]')
+print(notice_d1.text)
 
-
-element = browser.find_element_by_name('content')
-#WebDriverWait(browser, 5).until(
-#    EC.presence_of_element_located((By.ID, "content"))
-#  # Wait until the `text_to_score` element appear (up to 5 seconds)
-browser.execute_script('oEditors.getById["content"].setIR("11")')
-browser.find_element_by_name('form_w').submit()
-browser.implicitly_wait(3)
-browser.switch_to_frame('MyIFrame').switch_to_alert().accept();
-
-
-
-
-import pymongo
-
-conn = pymongo.MongoClient('mongodb://localhost:27017')
-db = conn.get_database('mongo_test')
-collection = db.get_collection('customer')
-
-collection.insert_one({"name":"cbchoi", "category":1, "region":'Daejeon'})
-
-results = collection.find()
-
-[print(result) for result in results]
-'''
+# 쿠키 지우기
+time.sleep(2)
+browser.delete_all_cookies()
+time.sleep(5)
+#/html/body/table[1]/tbody/tr[2]/td/table/tbody/tr/td[3]/table/tbody/tr[4]/td/table/tbody/tr[1]
